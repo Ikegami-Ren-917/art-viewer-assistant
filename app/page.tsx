e343e09952227f4b1e17738c6ed255f5a928256b
Ren <esd62276@kwansei.ac.jp>
2025-12-19 00:36:11 +0900

    Set ViewerAssistant2 as home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Versions</h1>
-      <ul>
-        <li><a href="/v1">v1</a></li>
-        <li><a href="/v2">v2</a></li>
-        <li><a href="/v3">v3</a></li>
-      </ul>
-    </div>
-  );
+// app/page.tsx
+import ViewerAssistant2 from "./components/ViewerAssistant2";
+
+export default function Page() {
+  return <ViewerAssistant2 />;
 }
+
